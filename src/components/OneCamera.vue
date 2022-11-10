e3ea1b73d6962fdaf3f0846689e181ac9ca4ea33
NilsGarskog <93979268+NilsGarskog@users.noreply.github.com>
2022-11-10 23:37:01 +0100

    uppdatering

---
+++ src/components/OneCamera.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>{{ camera.name }}</h3>
+    <img v-bind:src= "camera.image" style="width: 200px; height: 200px">
+    <ul>
+      <li>{{ camera.cameraType }}</li>
+      <li>Leica <span id="mount">{{ camera.mount }}</span></li>
+    </ul>
+  </div>
+</template>
+
+  <script>
+export default {
+  name: "OneCamera",
+  props: {
+    camera: Object,
+  },
+};
+</script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+</style>
+  
